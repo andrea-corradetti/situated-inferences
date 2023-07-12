71499b79b6144531d10a95bd4dac83d11bea1a9b
Andrea Corradetti <andreacorradetti28@gmail.com>
2023-07-12 15:25:46 +0200

    Refactoring to kotlin

--- src/main/kotlin/situatedInference/ExplainIter.kt
+++ src/main/kotlin/situatedInference/ExplainIter.kt
@@ -1,181 +1,208 @@
-package situatedInference;
+package situatedInference
 
-import com.ontotext.trree.*;
-import com.ontotext.trree.query.QueryResultIterator;
-import com.ontotext.trree.query.StatementSource;
-import com.ontotext.trree.sdk.StatementIterator;
-import org.jetbrains.annotations.Contract;
-import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import proof.ProofPlugin;
+import com.ontotext.trree.*
+import com.ontotext.trree.query.QueryResultIterator
+import com.ontotext.trree.query.StatementSource
+import com.ontotext.trree.sdk.StatementIterator
+import org.slf4j.Logger
+import proof.ProofPlugin
 
-import java.util.*;
+class ExplainIter(
+    requestContext: SituatedInferenceContext, //id of bnode representing the explain operation
+    private var reificationId: Long,
+    explainId: Long,
+    statementToExplain: Quad,
+    explicitStatementProps: ExplicitStatementProps
+) : StatementIterator(), ReportSupportedSolution {
+    private val logger: Logger?
+    private val statementToExplain: Quad
+    private val contextResolver: ContextResolver
+    private var inferencer: AbstractInferencer
+    private var repositoryConnection // connection to the raw data to get only the AXIOM statements ???
+            : AbstractRepositoryConnection
+    private var isExplicit: Boolean
+    private var isDerivedFromSameAs: Boolean
+    private var explicitContext //FIXME possibly misguiding name
+            : Long
+    var solutions = mutableSetOf<Solution?>()
+    private var iter: Iterator<Solution?>? = null
+    private var currentSolution: Solution? = null
+    private var currentPremiseNo = -1
+    private var values: Quad? = null
 
-public class ExplainIter extends StatementIterator implements ReportSupportedSolution {
-    private final Logger logger;
-    private final Quad statementToExplain;
-    private final ContextResolver contextResolver;
-    AbstractInferencer inferencer;
-    AbstractRepositoryConnection repositoryConnection; // connection to the raw data to get only the AXIOM statements ???
-    long reificationId; //id of bnode representing the explain operation
-    boolean isExplicit;
-    boolean isDerivedFromSameAs;
-    long explicitContext; //FIXME possibly misguiding name
-    Set<Solution> solutions = new LinkedHashSet<>();
-    Iterator<Solution> iter;
-    Solution currentSolution = null;
-    int currentPremiseNo = -1;
-    Quad values;
-
-    public ExplainIter(SituatedInferenceContext requestContext, long reificationId, long explainId, Quad statementToExplain, ExplicitStatementProps explicitStatementProps) {
-        this.reificationId = reificationId;
-        this.subject = this.reificationId;
-        this.predicate = explainId;
-
-        this.statementToExplain = statementToExplain;
-
-        this.isExplicit = explicitStatementProps.isExplicit;
-        this.isDerivedFromSameAs = explicitStatementProps.isDerivedFromSameAs;
-        this.explicitContext = explicitStatementProps.explicitContext;
-        this.inferencer = requestContext.getInferencer();
-        this.repositoryConnection = requestContext.getRepositoryConnection();
-        this.logger = requestContext.getLogger();
-        this.contextResolver = new ContextResolver(repositoryConnection);
-        this.init();
+    init {
+        subject = reificationId
+        predicate = explainId
+        this.statementToExplain = statementToExplain
+        isExplicit = explicitStatementProps.isExplicit
+        isDerivedFromSameAs = explicitStatementProps.isDerivedFromSameAs
+        explicitContext = explicitStatementProps.explicitContext
+        inferencer = requestContext.inferencer
+        repositoryConnection = requestContext.repositoryConnection
+        logger = requestContext.logger
+        contextResolver = ContextResolver(repositoryConnection)
+        init()
     }
 
-    public Set<Solution> getSolutions() {
-        return solutions;
-    }
-
-    public void init() {
+    private fun init() {
         if (isExplicit) {
-            Quad antecedent = new Quad(statementToExplain.subject, statementToExplain.predicate, statementToExplain.object, explicitContext, 0);
-            currentSolution = new Solution("explicit", List.of(antecedent));
-            currentPremiseNo = 0;
-            iter = getEmptySolutionIterator();
+            val antecedent = Quad(
+                statementToExplain.subject,
+                statementToExplain.predicate,
+                statementToExplain.`object`,
+                explicitContext,
+                0
+            )
+            currentSolution = Solution("explicit", listOf(antecedent))
+            currentPremiseNo = 0
+            iter = emptySolutionIterator()
         } else {
-            inferencer.isSupported(statementToExplain.subject, statementToExplain.predicate, statementToExplain.object, 0, 0, this); //this method has a side effect that prompts the overridden report() method to populate our solution with antecedents. The strange this reference does exactly that. Sorry, I don't make the rules. No idea what the fourth parameter does. Good luck
-            iter = solutions.iterator();
-            if (iter.hasNext()) {
-                currentSolution = iter.next();
+            inferencer.isSupported(
+                statementToExplain.subject, statementToExplain.predicate, statementToExplain.`object`, 0, 0, this
+            ) //this method has a side effect that prompts the overridden report() method to populate our solution with antecedents. The strange this reference does exactly that. Sorry, I don't make the rules. No idea what the fourth parameter does. Good luck
+            iter = solutions.iterator()
+            if (iter!!.hasNext()) {
+                currentSolution = iter!!.next()
             }
             if (currentSolution != null) {
-                currentPremiseNo = 0;
+                currentPremiseNo = 0
             }
         }
     }
 
-    @NotNull
-    @Contract(value = " -> new", pure = true)
-    private Iterator<Solution> getEmptySolutionIterator() {
-        return new Iterator<>() {
-            @Override
-            public boolean hasNext() {
-                return false;
-            }
-
-            @Override
-            public Solution next() {
-                return null;
-            }
-        };
-    }
-
-
-    @Override
-    public boolean report(String ruleName, QueryResultIterator queryResultIterator) {
-        logger.debug("report rule {} for {},{},{}", ruleName, statementToExplain.subject, statementToExplain.predicate, statementToExplain.object);
-        Set<Quad> antecedents = new HashSet<>();
+    override fun report(ruleName: String, queryResultIterator: QueryResultIterator): Boolean {
+        logger!!.debug(
+            "report rule {} for {},{},{}",
+            ruleName,
+            statementToExplain.subject,
+            statementToExplain.predicate,
+            statementToExplain.`object`
+        )
+        val antecedents: MutableSet<Quad> = HashSet()
+        val solutionSets: MutableList<Set<Quad>> = ArrayList()
         while (queryResultIterator.hasNext()) {
-            if (queryResultIterator instanceof StatementSource source) {
-                Iterator<StatementIdIterator> sourceSolutionIterator = source.solution();
-
+            if (queryResultIterator is StatementSource) {
+                val sourceSolutionIterator: Iterator<StatementIdIterator> = queryResultIterator.solution()
                 while (sourceSolutionIterator.hasNext()) {
-                    try (StatementIdIterator antecedent = sourceSolutionIterator.next()) {
-                        boolean isSelfReferential = (antecedent.subj == statementToExplain.subject && antecedent.pred == statementToExplain.predicate && antecedent.obj == statementToExplain.object);
+                    sourceSolutionIterator.next().use { antecedent ->
+                        val isSelfReferential =
+                            antecedent.subj == statementToExplain.subject && antecedent.pred == statementToExplain.predicate && antecedent.obj == statementToExplain.`object`
                         if (isSelfReferential) {
-                            logger.debug("skipped - self referential");
-                            continue;
+                            logger.debug("skipped - self referential")
+                            return@use
                         }
+                        logger.debug("Antecedent default context = {}", antecedent.context)
+                        val antecedentWithAllContexts = contextResolver.getStatementWithAllContexts(
+                            Quad(
+                                antecedent.subj, antecedent.pred, antecedent.obj, antecedent.context, antecedent.status
+                            )
+                        )
+                        logger.debug("antecedents with all contexts {}", antecedentWithAllContexts)
+                        solutionSets.add(antecedentWithAllContexts)
 
-                        logger.debug("Iter default context = " + antecedent.context);
-
-                        Set<Quad> antecedentWithAllContexts = contextResolver.getAntecedentWithAllContexts(new Quad(antecedent.subj, antecedent.pred, antecedent.obj, antecedent.context, antecedent.status));
-                        logger.debug("antecedents with all contexts " + antecedentWithAllContexts);
-
-                        boolean isStatementInSameContext = antecedentWithAllContexts.stream().anyMatch(quad -> quad.context == statementToExplain.context);
+                        //TODO if explaining an implicit statement, it will always be empty. quadToExplain.context is -2 and will never match antecedents in graphs.
+                        if (statementToExplain.context == SystemGraphs.IMPLICIT_GRAPH.id.toLong()) {
+                        }
+                        val isStatementInSameContext = antecedentWithAllContexts.stream()
+                            .anyMatch { (_, _, _, context1): Quad -> context1 == statementToExplain.context }
                         if (isStatementInSameContext) {
-                            logger.debug("statement is same context {}", statementToExplain.context);
-                            antecedents.add(new Quad(antecedent.subj, antecedent.pred, antecedent.obj, statementToExplain.context, antecedent.status));
+                            logger.debug("statement is same context {}", statementToExplain.context)
+                            antecedents.add(
+                                Quad(
+                                    antecedent.subj,
+                                    antecedent.pred,
+                                    antecedent.obj,
+                                    statementToExplain.context,
+                                    antecedent.status
+                                )
+                            )
                         }
-
-                        boolean isStatementInDefaultGraph = !isStatementInSameContext && antecedentWithAllContexts.stream().anyMatch(quad -> quad.context == SystemGraphs.EXPLICIT_GRAPH.getId());
+                        val isStatementInDefaultGraph = !isStatementInSameContext && antecedentWithAllContexts.stream()
+                            .anyMatch { (_, _, _, context1): Quad -> context1 == SystemGraphs.EXPLICIT_GRAPH.id.toLong() }
                         if (ProofPlugin.isSharedKnowledgeInDefaultGraph && isStatementInDefaultGraph) {
-                            logger.debug("statement is in default graph");
-                            antecedents.add(new Quad(antecedent.subj, antecedent.pred, antecedent.obj, SystemGraphs.EXPLICIT_GRAPH.getId(), antecedent.status));
+                            logger.debug("statement is in default graph")
+                            antecedents.add(
+                                Quad(
+                                    antecedent.subj,
+                                    antecedent.pred,
+                                    antecedent.obj,
+                                    SystemGraphs.EXPLICIT_GRAPH.id.toLong(),
+                                    antecedent.status
+                                )
+                            )
                         }
-
-                        boolean isStatementInScope = isStatementInSameContext || (ProofPlugin.isSharedKnowledgeInDefaultGraph && isStatementInDefaultGraph);
-
-                        boolean isStatementOnlyImplicit = !isStatementInScope && antecedentWithAllContexts.stream().allMatch(quad -> quad.context == SystemGraphs.IMPLICIT_GRAPH.getId());
+                        val isStatementInScope =
+                            isStatementInSameContext || ProofPlugin.isSharedKnowledgeInDefaultGraph && isStatementInDefaultGraph
+                        val isStatementOnlyImplicit = !isStatementInScope && antecedentWithAllContexts.stream()
+                            .allMatch { (_, _, _, context1): Quad -> context1 == SystemGraphs.IMPLICIT_GRAPH.id.toLong() }
                         if (isStatementOnlyImplicit) {
-                            logger.debug("statement is only implicit");
-                            antecedents.add(new Quad(antecedent.subj, antecedent.pred, antecedent.obj, SystemGraphs.IMPLICIT_GRAPH.getId(), antecedent.status));
+                            logger.debug("statement is only implicit")
+                            antecedents.add(
+                                Quad(
+                                    antecedent.subj,
+                                    antecedent.pred,
+                                    antecedent.obj,
+                                    SystemGraphs.IMPLICIT_GRAPH.id.toLong(),
+                                    antecedent.status
+                                )
+                            )
                         }
-
                         if (!isStatementInScope && !isStatementOnlyImplicit) {
-                            logger.debug("statement {},{},{} is out of scope", antecedent.subj, antecedent.pred, antecedent.obj);
-                            return false;
+                            logger.debug(
+                                "statement {},{},{} is out of scope", antecedent.subj, antecedent.pred, antecedent.obj
+                            )
+                            return false
                         }
-                        logger.debug("Saved antecedents " + antecedents);
+                        logger.debug("Saved antecedents {}", antecedents)
                     }
                 }
             }
-            queryResultIterator.next();
+            queryResultIterator.next()
         }
-
-//        Set<Quad> filteredAntecedents = antecedents.stream().filter(quad -> areAntecedentsInScope(quad)).collect(Collectors.toSet());
-
-        boolean areAllAntecedentsImplicit = antecedents.stream().allMatch(quad -> quad.context == SystemGraphs.IMPLICIT_GRAPH.getId());
+        val areAllAntecedentsImplicit = antecedents.all {quad -> quad.context.toInt() == SystemGraphs.IMPLICIT_GRAPH.id }
         if (areAllAntecedentsImplicit) {
             logger.debug("All antecedents are implicit");
             return false;
         }
 
-//        List<long[]> antecedentsAsArrays = antecedents.stream().map(quad -> new long[]{quad.subject, quad.predicate, quad.object, quad.context}).collect(Collectors.toList());
-        Solution solution = new Solution(ruleName, antecedents.stream().toList());
-        boolean added = solutions.add(solution);
-        logger.debug(added ? "added" : "already added");
-        return false;
+        val solution = Solution(ruleName, antecedents.toList());
+        val added = solutions.add(solution)
+        logger.debug(if (added ) "added" else "already added");
+        return false
     }
 
-
-    @Override
-    public void close() {
-        currentSolution = null;
-        solutions = null;
+    override fun close() {
+        currentSolution = null
+        solutions.clear()
     }
 
-
-    @Override
-    public boolean next() {
+    override fun next(): Boolean {
         while (currentSolution != null) {
-            if (currentPremiseNo < currentSolution.antecedents.size()) {
-                values = currentSolution.antecedents.get(currentPremiseNo++);
-                return true;
+            if (currentPremiseNo < currentSolution!!.antecedents.size) {
+                values = currentSolution!!.antecedents[currentPremiseNo++]
+                return true
             }
-
-            values = null;
-            currentPremiseNo = 0;
-            currentSolution = iter.hasNext() ? iter.next() : null;
+            values = null
+            currentPremiseNo = 0
+            currentSolution = if (iter!!.hasNext()) iter!!.next() else null
         }
-        return false;
+        return false
     }
 
-    @Override
-    public AbstractRepositoryConnection getConnection() {
-        return repositoryConnection;
+    override fun getConnection(): AbstractRepositoryConnection {
+        return repositoryConnection
     }
 
+    companion object {
+        private fun emptySolutionIterator(): Iterator<Solution?> =
+            object : Iterator<Solution?> {
+                override fun hasNext(): Boolean {
+                    return false
+                }
+
+                override fun next(): Solution? {
+                    return null
+                }
+            }
+    }
 }
