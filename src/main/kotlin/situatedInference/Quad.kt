d6d3ff5802b0b8b01e1a76c2495dbc06caa9c429
Andrea Corradetti <andreacorradetti28@gmail.com>
2023-07-20 16:31:21 +0200

    Add Triple class

--- src/main/kotlin/situatedInference/Quad.kt
+++ src/main/kotlin/situatedInference/Quad.kt
@@ -1,5 +1,7 @@
 package situatedInference
 
+import com.ontotext.trree.StatementIdIterator
+
 data class Quad(
     @JvmField val subject: Long,
     @JvmField val predicate: Long,
@@ -8,4 +10,23 @@ data class Quad(
     @JvmField val status: Int = 0,
 ) {
     constructor(array: LongArray) : this(array[0], array[1], array[2], array[3])
+    constructor(iterator: StatementIdIterator) : this(
+        iterator.subj,
+        iterator.pred,
+        iterator.obj,
+        iterator.context,
+        iterator.status
+    )
+
+    fun isSameTripleAs(other: Quad): Boolean =
+        (subject == other.subject) && (predicate == other.predicate) && (`object` == other.`object`)
+
+    fun asTriple() = Triple(subject, predicate, `object`)
 }
+
+
+data class Triple(
+    @JvmField val subject: Long,
+    @JvmField val predicate: Long,
+    @JvmField val `object`: Long,
+)
