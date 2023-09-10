1ed61b37f169d373b866e64d70a0e9164d3d7686
Andrea Corradetti <andreacorradetti28@gmail.com>
2023-09-10 17:51:38 +0200

    delete useless file

--- src/main/kotlin/situatedInference/PluginConsistencyException.kt
+++
@@ -1,6 +0,0 @@
-package situatedInference
-
-import com.ontotext.trree.sdk.PluginException
-
-class PluginConsistencyException(message: String?) : PluginException("Consistency Exception: $message")
-
